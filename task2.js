9c0ed4ff0a04fb8218ec89e63c0ebda8f25b22eb
RostyslavGrabar <rostikgrabar@gmail.com>
2020-10-14 13:13:30 +0300

    task2 comlete

--- task2.js
+++ task2.js
@@ -2,5 +2,15 @@
 // а також видалити частину рядка, починаючи з останнього входження цього символу і до кінця.
 
 function countCharsInString(str, char){
+    let arrayLowerChars = str.split("").map(item => {
+        if(item == char) item = item.toUpperCase();
+        return item;
+    })
+    let resultStr = arrayLowerChars.join('');
+    let lastCharIndex = resultStr.lastIndexOf(char.toUpperCase());
+    resultStr = resultStr.substr(0,lastCharIndex);
 
+    console.log(`String: "${str}" \nChar: "${char}" \n\nResult: ${resultStr}`);
 }
+
+countCharsInString("Користувач вводить рядок і символ. У рядку знайти всі входження цього символу і перевести його в верхній регістр, а також видалити частину рядка, починаючи з останнього входження цього символу і до кінця." , 'і')
